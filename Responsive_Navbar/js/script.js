6f828c72bc2fd4a7fbc48932dd1c45ab33a10b0d
Stanley Roy <tutorjoes@gmail.com>
2022-09-15 11:01:42 +0530

    Tutor Joes Free Bootcamp

---
+++ Responsive_Navbar/js/script.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+  const menu_btn=document.querySelector('.right-bar');
+  const mobile_nav=document.querySelector('.mobile-nav');
+
+  menu_btn.addEventListener('click',function(){
+    menu_btn.classList.toggle('is-active');
+    mobile_nav.classList.toggle('is-active');
+  });
+};
